f465f1a81e1dcc3430c72e4902d2353a8a7ada27
sandratraniaina <sandratrarafa@gmail.com>
2024-12-17 20:24:29 +0300

    feat: Create isValidPassword function

--- utils/validators.js
+++ utils/validators.js
@@ -6,3 +6,9 @@ const isValidName = (name) => {
     const nameRegex = /^[A-Za-z\s]+$/;
     return name && nameRegex.test(name);
 };
+
+const isValidPassword = (pwd) => {
+    const hasUppercase = /[A-Z]/.test(pwd);
+    const hasNumber = /\d/.test(pwd);
+    return pwd && pwd.length >= 8 && hasUppercase && hasNumber;
+};
