31f170a27c11e2840986dc90161ae15414b199e5
sandratraniaina <sandratrarafa@gmail.com>
2024-12-17 20:22:48 +0300

    feat: Create isValidName function

--- utils/validators.js
+++ utils/validators.js
@@ -1,3 +1,8 @@
 const isValidUsername = (username) => {
     return (username && username.length >= 3);
 }
+
+const isValidName = (name) => {
+    const nameRegex = /^[A-Za-z\s]+$/;
+    return name && nameRegex.test(name);
+};
