3998d4803531265cfb326424227715825687f943
sandratraniaina <sandratrarafa@gmail.com>
2024-12-17 21:05:47 +0300

    feat: Add validateSignup middleware in authroutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const { signup } = require('../controllers/authControllers');
+const express = require("express");
+const { signup } = require("../controllers/authControllers");
+const { validateSignup } = require("../middlewares/authMiddlewares");
 
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post("/signup", validateSignup, signup);
 
 module.exports = router;
