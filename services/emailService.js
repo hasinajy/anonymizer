92ccd0b90530a5a826e4cf8c02b4c5f31a8c8278
sandratraniaina <sandratrarafa@gmail.com>
2024-12-18 22:39:43 +0300

    feat: Create sendEmail function

---
+++ services/emailService.js
@@ -0,0 +1,21 @@
+const nodemailer = require("nodemailer");
+
+require("dotenv").config();
+
+const sendEmail = async (mailOptions) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+        tls: {
+            rejectUnauthorized: false,
+        },
+    });
+
+    return await transporter.sendMail(mailOptions);
+};
