d8b7031e42506c5866505426615fc22bc73309ef
sandratraniaina <sandratrarafa@gmail.com>
2024-12-18 23:01:49 +0300

    feat: Create isValidToken function

---
+++ utils/tokenUtils.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken");
+
+const isValidToken = (token) => {
+    try {
+        jwt.verify(token, process.env.JWT_SECRET);
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
