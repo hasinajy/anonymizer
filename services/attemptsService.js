bbb56fcc8a430f85754997a684ddc42381a0d5f4
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-12-19 10:32:16 +0300

    feat: create resetAttempts function

--- services/attemptsService.js
+++ services/attemptsService.js
@@ -1,15 +1,26 @@
-const poot = require('../config/bd');
+const pool = require('../config/bd');
 
-const canAuthenticate = async (email) =>{
-    const query = 'SELECT COUNT(*) as email_count FROM account WHERE email = $1 AND attempts > 0;';
-    const values = [email]; 
-    try{
+const canAuthenticate = async (email) => {
+    const query = 'SELECT COUNT(*) as email_count FROM account WHERE email = $1 AND attempts > 0';
+    const values = [email];
+    try {
         const result = await poot.query(query, values);
-        return result.rows[0].email_count > 0 ; 
-    }catch(err){
+        return result.rows[0].email_count > 0;
+    } catch (err) {
         console.error('Database error:', err);
         throw new Error('Failed to check email');
     }
 };
 
-module.exports = canAuthenticate ; 
+const resetAttempts = async (email) => {
+    const query = 'Update account set attempts = 3 where email = $1 ';
+    const values = [email];
+    try {
+        const result = await pool.query(query ,values);
+    } catch (err) {
+        console.error('Database error:', err);
+        throw new Error('Failed to check email');
+    }
+}
+
+module.exports = { canAuthenticate, resetAttempts }; 
