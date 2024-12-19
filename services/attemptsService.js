6b9e1f8d1a68d5fac68575026ece99b93d47908f
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-12-19 16:56:13 +0300

    refactor: Move function deleteUser in the file userModel

--- services/attemptsService.js
+++ services/attemptsService.js
@@ -25,28 +25,4 @@ const resetAttempts = async (email) => {
     }
 }
 
-
-const deleteUser = async (email) => {
-    const deleteAccountQuery = 'DELETE FROM account WHERE email = $1 RETURNING person_id';
-    const deletePersonQuery = 'DELETE FROM person WHERE person_id = $1';
-    const values = [email];
-
-    try {
-        const result = await pool.query(deleteAccountQuery, values);
-
-        if (result.rowCount === 0) {
-            throw new Error('No account found with the provided email');
-        }
-
-        const personId = result.rows[0].person_id;
-
-        await pool.query(deletePersonQuery, [personId]);
-        return { message: 'Account and associated person deleted successfully' };
-    } catch (err) {
-        console.error('Database error:', err);
-        throw new Error('Failed to delete account and person');
-    }
-};
-
-
-module.exports = { canAuthenticate, resetAttempts , deleteUser }; 
+module.exports = { canAuthenticate, resetAttempts }; 
