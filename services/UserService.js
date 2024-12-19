9f6c6d9fbb60e65614da1f323215daeb940a4eb3
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-12-19 15:07:43 +0300

    feat: Create updatePassword function

---
+++ services/UserService.js
@@ -0,0 +1,13 @@
+const pool = require('../config/bd');
+const { hashPassword } = require('../utils/hashPassword');
+
+const updatePaswword = async (email, password) => {
+    const query = 'Update account set password = $1 where email = $2';
+    const values = [email,hashPassword(password)];
+    try {
+        await pool.query(query, values);
+    } catch (error) {
+        console.log('Error for updating password ');
+        throw new Error('Faild to update password ');
+    }
+}
