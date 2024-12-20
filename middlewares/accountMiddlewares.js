6041802000dbf2ae9f059fd6a2b8217ac2f73e17
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-20 11:04:11 +0300

    feat : add middleware for update password account

--- middlewares/accountMiddlewares.js
+++ middlewares/accountMiddlewares.js
@@ -1,6 +1,6 @@
 const { getTokenFromHeaders, isValidToken } = require("../utils/tokenUtils");
 const { sendResponse } = require("../utils/responseHandler");
-const { isValidUsername } = require("../utils/validators");
+const { isValidPassword } = require("../utils/validators");
 
 const validateDeleteAccount = (req, res, next) => {
     const accountId = req.params.accountId;
@@ -62,7 +62,40 @@ const validateUpdateAccount = (req, res, next) => {
     next();
 };
 
+const validateUpdatePasswordAccount = (req, res, next) => {
+    const accountId = req.params.accountId;
+    const { newPassword }= req.body;
+    const token = getTokenFromHeaders(req);
+
+    let errors = [];
+
+    if(token == null){
+        errors.push("Token required for this request");
+    }
+    
+    if(!isValidToken(token)){
+        errors.push("Token is not valid");
+    }
+    
+    if (!accountId) {
+       errors.push("Account ID is required.");
+    }
+
+    if(!isValidPassword(newPassword)){
+        errors.push("Format not valid for username")
+    }
+
+    if (errors) {
+        return sendResponse(res, 401, false, "Invalid data from request", {
+            errors: errors,
+        });
+    }
+
+    next();
+};
+
 module.exports = {
     validateDeleteAccount,
-    validateUpdateAccount
+    validateUpdateAccount,
+    validateUpdatePasswordAccount
  };
