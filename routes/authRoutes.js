cb73fcb91d42b505660b3212f5792fb7c6b420a0
hasinajy <jyhasina@gmail.com>
2024-12-20 11:01:07 +0300

    feat: Implement jwt token validation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { signup, validateSignUp, signIn } = require("../controllers/authControllers");
-const { validateSignup } = require("../middlewares/authMiddlewares");
+const { signup, validateSignUp, signIn, validateToken } = require("../controllers/authControllers");
+const { validateSignup, authenticateToken } = require("../middlewares/authMiddlewares");
 
 const router = express.Router();
 
 router.post("/signup", validateSignup, signup);
 router.get("/signup/:accountId", validateSignUp);
 router.post('/signin', signIn);
+router.get('/validate-token', authenticateToken, validateToken);
 
 module.exports = router;
