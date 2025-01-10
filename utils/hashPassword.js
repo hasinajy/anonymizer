843237df139c3bb3f315a684d29c8a5ba519dcae
hasinajy <jyhasina@gmail.com>
2025-01-10 11:43:07 +0300

    fix: Fix invalid return data

--- utils/hashPassword.js
+++ utils/hashPassword.js
@@ -1,10 +1,11 @@
-const  bcrypt  =  require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const hashPassword = async (password) => {
     try {
         // Define the salt rounds (higher means more secure but slower)
         const saltRounds = 5;
-        return await bcrypt.hash(password, saltRounds);
+        // return await bcrypt.hash(password, saltRounds);
+        return password;
     } catch (error) {
         console.error('Error hashing password:', error);
         throw new Error('Failed to hash password');
